12e65f94e2dfb820a7ca451ad336fa5686bc2409
Crigerprogrammer <crigerprogrammer@gmail.com>
2018-12-03 17:23:26 -0600

    Funciones en Python

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-clients = 'cristian,hernandez,'
+clients = 'Cristian,Hernandez,'
+
+def create_client(client_name):
+	global clients
+	clients += client_name
+	_add_comma()
+
+
+def list_clients():
+	global clients
+	print(clients)
+
+
+
+def _add_comma():
+	global clients
+	clients += ','
+
 
 if __name__ == '__main__':
-	clients += 'gerardo'
-	print(clients)
+	list_clients()
+	create_client('Gerardo')
+	list_clients()
